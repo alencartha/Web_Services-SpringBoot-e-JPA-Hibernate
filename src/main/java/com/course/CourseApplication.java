4e7fd4e8d409abea770d33ac5e3a6d3aee0d0aca
alencartha <alencar_tha@hotmail.com>
2021-09-05 18:06:59 -0300

    H2 database, test profile, JPA

--- src/main/java/com/course/CourseApplication.java
+++ src/main/java/com/course/CourseApplication.java
@@ -2,8 +2,10 @@ package com.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan ( basePackages = { "com.course" })
 public class CourseApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/course/resources/UserResource.java
+++ src/main/java/com/course/resources/UserResource.java
@@ -1,7 +1,6 @@
 package com.course.resources;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
