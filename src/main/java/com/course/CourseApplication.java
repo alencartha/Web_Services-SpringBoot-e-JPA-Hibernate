32f0dcefe8883917368dcd5a0b01bc9eb1e9c4f1
alencartha <alencar_tha@hotmail.com>
2021-09-05 18:42:44 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/course/CourseApplication.java
+++ src/main/java/com/course/CourseApplication.java
@@ -2,10 +2,9 @@ package com.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
-@ComponentScan ( basePackages = { "com.course" })
 public class CourseApplication {
 
 	public static void main(String[] args) {
